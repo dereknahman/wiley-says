40d75411ad69bd7b3bd8bdb4e90e954148ca02d7
kirsty.simmonds <kirsty.simmonds@piavita.com>
2019-11-03 15:10:54 +0100

    done

---
+++ bin.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+
+const wileyQuotes = [
+	"hold tight Bashy suck your mum, on Christmas Day, hold tight everyone at home smiling suck your mum",
+	"i've never bumped into a person and felt their thing",
+	"the mic keeps cutting out...should really get someone to look into that...hang tight the management",
+	"i got my baby mum some slippers.....petrol station, 2 in the morning, saw some, i thought, yeah, slippers",
+	"If you dissed me while i was in jail i wiil run up to ya old gran in the super market and say boo so loud she will shit her codweb knickers",
+	"Hold tite Devlinofficial for makin an album full of tunes for 12 year old scouts to put on their phones",
+	"I’m more reckless than your mums African shoes at the bottom of the stairs",
+	"i just blocked stevie wonder lol",
+	"If u dare have a bbq invite me dont be out of order …any endz it dont matter ….man will get there",
+	"“NID FOR SPID”",
+	"Nid for spid 2",
+	"your mum stinks of ralgex",
+	"Don't go in ya girls phone tho don't ever try and know what's in her brain u will cry even if she loves u if she met drake 2mora its a wrap",
+	"I got a breakfast in scotland and they had square sausage ...Trust me fam ….square",
+	"Why has this cat decided to put a chrisp packet on Its head",
+	"People think I'm mad. I understand why, but I'm not mad. I am actually quite sane",
+	"hold tight the man that dont like me",
+	"If I saw a chicken... outside... at night... I'd feed it chicken bones",
+	"revenge is sweet like 3 sugars in a cup of tea",
+	"hold tite anyone, take it on board if you want",
+	"Why would I be in an alleyway in Hackney?...I have learned",
+	"Tell ya dad to stop charging extra 50 p for milk he is a knock",
+	"if i see a spider in my room i wont kill it",
+	"Sharkey claims he had a hard time, Sharkey is a normal boy. He lives in Upney, he's an indirect spitter, he got 10 GCSEs. He's a boffin",
+	"Hold tight all internet gangsters ...ask yourself this `Are you as rough as you type`",
+	"your nan is a lorry driver",
+	"holdtight all dem people at home smiling yeah. Suck your mum. take the smile off your face now",
+	"both ur parents are experiencing the credit crunch i aint",
+	"All year round I been a nice person then I found out everyone was a PRICK",
+	"Thanks for my mum for giving birth to me so I can wile up all these donut mcs",
+	"im not 40 u dusty tramp go tell ya mum i said your house smells of mash potato",
+	"If you a big fellow take care on jet ski’s",
+	"I wish people thought, this guy's been going for years. Let's give him a break"
+]
+
+const item = wileyQuotes[Math.floor(Math.random()*wileyQuotes.length)];
+console.log(item)
